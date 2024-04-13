27b1f68b356277151fa8f569a6c679f9a78be1e8
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-04-13 13:14:31 +0200

    authenticate middleware

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,12 +3,13 @@ import express from 'express';
 import { login, register, logout, current } from '../controllers/usersControllers';
 import validateBody from '../helpers/validateBody.js';
 import { registerSchema, loginSchema } from '../schemas/usersSchemas.js';
+import { authenticate } from '../helpers/authenticate.js';
 
 const authRouter = express.Router();
 
-authRouter.get("/current", current);
+authRouter.get("/current", authenticate, current);
 authRouter.post("/register", validateBody(registerSchema), register);
 authRouter.post('/login', validateBody(loginSchema), login);
-authRouter.post('/logout', logout);
+authRouter.post('/logout', authenticate, logout);
 
 export default authRouter;
